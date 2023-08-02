3e36fc6a6d13798171bc75c4703e4321d5723aaa
manhtri <manhtri@bee-system.vn>
2023-08-02 09:17:56 +0700

    initial project

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        // CORS Configuration (Cross-Origin Resource Sharing)
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()        //WithOrigins("https://example.com")
+                    .AllowAnyMethod()               //WithMethods("POST", "GET")
+                    .AllowAnyHeader());             //WithHeaders("accept", "content-type")
+            });
+
+        // IIS Configuration
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,40 @@
+﻿using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+// builder thuộc type WebApplicationBuilder
+/* WebApplicationBuilder
+    - Thêm cấu hình cho dự án bằng cách sử dụng thuộc tính builder.Configuration
+    - Đăng ký services với builder.Services
+    - Cấu hình ghi log với thuộc tính builder.Logging
+    - Cấu hình IHostBuilder và IWebHostBuilder
+*/
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
